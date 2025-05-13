8d96c70c3dcf1e3977d5b0aad760c91c9948b847
Samuel Hellstrøm <samuel.hellstrom@knowit.no>
2025-05-13 11:54:31 +0200

    Initial Commit

---
+++ Booking/BookingApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookingApp.swift
+//  Booking
+//
+//  Created by Samuel Hellstrøm on 13/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct BookingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BookingTests/BookingTests.swift
@@ -0,0 +1,16 @@
+//
+//  BookingTests.swift
+//  BookingTests
+//
+//  Created by Samuel Hellstrøm on 13/05/2025.
+//
+
+import Testing
+
+struct BookingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

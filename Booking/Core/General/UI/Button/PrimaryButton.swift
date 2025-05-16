e8faf9e32cc53091d6ae42b5c2a74ec2e6054333
Samuel Hellstrøm <samuel.hellstrom@knowit.no>
2025-05-16 09:09:22 +0200

    Added PrimaryButton and SecondaryButton

---
+++ Booking/Core/General/UI/Button/PrimaryButton.swift
@@ -0,0 +1,42 @@
+
+//
+//  PrimaryButton.swift
+//  Booking
+//
+//  Created by Samuel Hellstrøm on 16/05/2025.
+//
+
+import SwiftUI
+
+struct PrimaryButton: View {
+    let text: String
+    var action: () -> Void
+    var fullWidth: Bool = true
+    var controlSize: ControlSize = .regular
+    var disabled: Bool = false
+    var cornerRadius: CGFloat = 18
+
+    var body: some View {
+        Button(action: action) {
+            Text(text)
+                .padding(10)
+                .frame(maxWidth: fullWidth ? .infinity : nil)
+                .fontWeight(.semibold)
+        }
+        .buttonStyle(.borderedProminent)
+        .controlSize(controlSize)
+        .disabled(disabled)
+        .cornerRadius(cornerRadius, antialiased: true)
+    }
+}
+
+struct PrimaryButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        VStack(spacing: 20) {
+            PrimaryButton(text: "Primary button", action: {})
+            PrimaryButton(text: "Primary button", action: {}, disabled: true)
+            PrimaryButton(text: "Non full-width", action: {}, fullWidth: false)
+        }
+        .padding(10)
+    }
+}
